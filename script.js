f41894e248ed4134ae7b6c73177a30bca7e798b0
Joshua Whitney <83869839+NDSheep@users.noreply.github.com>
2024-12-12 10:18:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ const gameState = {
             sprite: 'url("aidle.gif")',
             attacks: [
                 { name: "poke", damage: 10, sprite: 'url("aatk.gif")' },
-            ]
+            ],
+            questionSet: 0,
         },
         schnP1: {
             name: "Captain Schneider",
@@ -27,7 +28,8 @@ const gameState = {
                 { name: "illegal", damage: 20, sprite: 'url("sp1ill.gif")' },
                 { name: "take a lap", damage: 16, sprite: 'url("sp1lap.gif")' },
                 { name: "you forgot the constant of integration", damage: 24, sprite: 'url("sp1const.gif")' }
-            ]
+            ],
+            questionSet: 1,
         },
         schnP2: {
             name: "Lord of Calculus, Controller of the tangent line, Sir Schneider",
@@ -37,18 +39,122 @@ const gameState = {
             sprite: 'url("sp2idle.gif")',
             attacks: [
                 { name: "find the tangent line", damage: 28, sprite: 'url("sp2idle.gif")' },
-            ]
+            ],
+            questionSet: 2,
         },
     },
     questionBank: [
-        {
-            question: "What is 2 + 2?",
-            answer: "4"
-        },
-        {
-            question: "What is the capital of France?",
-            answer: "Paris"
-        },
+        [ // Question set 0
+            {
+                question: "∫cos(x)dx =",
+                answer: "sin(x)+C"
+            },
+            {
+                question: "∫sin(x)dx =",
+                answer: "-cos(x)+C"
+            },
+            {
+                question: "∫cos(x)dx =",
+                answer: "sin(x)+C"
+            },
+            {
+                question: "∫sec^2(x)dx =",
+                answer: "tan(x)+C"
+            },
+            {
+                question: "∫sec(x)tan(x)dx =",
+                answer: "sec(x)+C"
+            },
+            {
+                question: "∫csc(x)cot(x)dx =",
+                answer: "-csc(x)+C"
+            },
+            {
+                question: "∫csc^2(x)dx =",
+                answer: "-cot(x)+C"
+            },
+            {
+                question: "d/dx[sin(x)] =",
+                answer: "cos(x)"
+            },
+            {
+                question: "d/dx[cos(x)] =",
+                answer: "-sin(x)"
+            },
+            {
+                question: "d/dx[tan(x)] =",
+                answer: "sec^2(x)"
+            },
+            {
+                question: "d/dx[sec(x)] =",
+                answer: "sec(x)tan(x)"
+            },
+            {
+                question: "d/dx[csc(x)] =",
+                answer: "-csc(x)cot(x)"
+            },
+            {
+                question: "d/dx[cot(x)] =",
+                answer: "-csc^2(x)"
+            },
+        ],
+        [ // Question set 1
+            {
+                question: "d/dx[sin(x^2)] =",
+                answer: "2xcos(x^2)"
+            },
+            {
+                question: "∫sin(2x)dx =",
+                answer: "-1/2cos(2x)+C"
+            },
+            {
+                question: "d/dx[csc(x)-sin(x)] =",
+                answer: "-csc(x)cot(x)-cos(x)"
+            },
+            {
+                question: "∫[tan^2(x)+1]dx =",
+                answer: "tan(x)+C"
+            },
+            {
+                question: "d/dx[sin(pix)] =",
+                answer: "picos(pix)"
+            },
+            {
+                question: "∫cos(x)+sin(x)dx =",
+                answer: "cos(x)-sin(x)"
+            },
+            {
+                question: "d/dx[cos(x)csc(x)] =",
+                answer: "-csc^2(x)"
+            },
+            {
+                question: "∫[sin^2(x)+cos^2(x)]dx =",
+                answer: "x+C"
+            },
+            
+        ],
+        [ // Question set 2
+            {
+                question: "d/dx[cot(y)=x-y], find dy/dx",
+                answer: "1/[1-csc^2(y)]"
+            },
+            {
+                question: "∫sin(x)cos(x)dx =",
+                answer: "-1/4cos(2x)+C"
+            },
+            {
+                question: "d/dx[sin(x)+cos(y)=1], find dy/dx",
+                answer: "cos(x)/sin(y)"
+            },
+            {
+                question: "∫sin^2(x)dx =",
+                answer: "x/2-(sin(2x))/4+C"
+            },
+            {
+                question: "d/dx[xtan(y)=2], find dy/dx",
+                answer: "-tan(y)/[xsec^2(y)]"
+            },
+        ]
         // Add more questions here
     ],
     scenes: [
@@ -56,7 +162,7 @@ const gameState = {
             background: 'url("bg.png")',
             character: 'url("title.png")',
             name: "By Joshua Whitney and Julian Foley",
-            dialogue: "An interactive experience with the purpose of teaching a strong fondations for the integration and differentiation of trigonometric functions.",
+            dialogue: "An interactive experience to teach a strong foundations for the integration and differentiation of trigonometric functions.",
             choices: [
                 { text: "Continue", nextScene: 1 }
             ]
@@ -65,7 +171,7 @@ const gameState = {
             background: 'url("bg.png")',
             character: 'url("sidle.gif")',
             name: "Mission Captain Schneider",
-            dialogue: "You have been selected this highstakes mission to escape the distant planet and return to Earth.",
+            dialogue: "You have been selected this high-stakes mission to escape the distant planet and return to Earth.",
             choices: [
                 { text: "Continue", nextScene: 2 }
             ]
@@ -74,7 +180,7 @@ const gameState = {
             background: 'url("bg.png")',
             character: 'url("sidle.gif")',
             name: "Mission Captain Schneider",
-            dialogue: "To do this you must re-aquire the lost part of our ship, the mythical \"Tangent Line\".",
+            dialogue: "To do this, you must re-acquire the lost part of our ship, the mythical \"Tangent Line\".",
             choices: [
                 { text: "Continue", nextScene: 3 },
             ]
@@ -83,7 +189,7 @@ const gameState = {
             background: 'url("bg.png")',
             character: 'url("sidle.gif")',
             name: "Mission Captain Schneider",
-            dialogue: "To do this I need you to defeat the allien leader and retrieve the Tangent Line. Good luck!",
+            dialogue: "To do this, I need you to defeat the alien leader and retrieve the Tangent Line. Good luck!",
             choices: [
                 { text: "Continue", nextScene: 4 },
             ]
@@ -101,7 +207,7 @@ const gameState = {
             background: 'url("bg.png")',
             character: 'url("sidle.gif")',
             name: "Mission Captain Schneider",
-            dialogue: "Oh wait I forgot to mention, you will need this",
+            dialogue: "Oh wait, I forgot to mention, you will need this",
             choices: [
                 { text: "Continue", nextScene: 6 },
             ]
@@ -110,16 +216,16 @@ const gameState = {
             background: 'url("bg.png")',
             character: 'url("t.gif")',
             name: "Narrator",
-            dialogue: "He hands you the legendary Sword of Integration, the Shield of Differentiation and the book of Trigonomotry. You are now ready to face the alien leader Once you learn to used them.",
+            dialogue: "He hands you the legendary Sword of Integration, the Shield of Differentiation, and the book of Trigonometry. You are now ready to face the alien leader Once you learn to use them.",
             choices: [
-                { text: "Read the book of Trigonomotry", nextScene: 7},
+                { text: "Read the book of Trigonometry", nextScene: 7},
                 { text: "Equip the tools and head forth", nextScene: 9}
             ]
         },
         {
             background: 'url("bg.png")',
             character: 'url("t.gif")',
-            name: "Book of Trigonomotry",
+            name: "Book of Trigonometry",
             dialogue: "Trigonometric derivitives:\n   d/dx\[sin(u)\] = cos(u)u\'\n   d/dx\[cos(u)\] = -sin(u)u\'\n  d/dx\[tan(u)\] = sec^2(u)u\'\n   d/dx\[sec(u)\] = sec(u)tan(u)u\'\n   d/dx\[csc(u)\] = -csc(u)cot(u)u\'\n   d/dx\[cot(u)\] = -csc^2(u)u\'",
             choices: [
                 { text: "Flip to the next page", nextScene: 8 },
@@ -128,7 +234,7 @@ const gameState = {
         {
             background: 'url("bg.png")',
             character: 'url("t.gif")',
-            name: "Book of Trigonomotry",
+            name: "Book of Trigonometry",
             dialogue: "Trigonometric integrals:\n   ∫sin(u)du = -cos(u) + C\n   ∫cos(u)du = sin(u) + C\n   ∫sec^2(u)dx =        tan(u)+C\n   ∫sec(u)tan(u)dx = sec(u)+C\n   ∫csc(u)cot(u)dx = -csc(u)+C\n   ∫csc^2(u)dx = -cot(u)+C",
             choices: [
                 { text: "Close the book and equip the tools.", nextScene: 9 },
@@ -262,7 +368,7 @@ function updateCombatStats() {
 }
 
 function askQuestion(action) {
-    const question = gameState.questionBank[Math.floor(Math.random() * gameState.questionBank.length)];
+    const question = gameState.questionBank[gameState.currentEnemy.questionSet][Math.floor(Math.random() * gameState.questionBank[gameState.currentEnemy.questionSet].length)];
     const userAnswer = prompt(question.question);
     if (userAnswer && userAnswer.toLowerCase().trim() === question.answer.toLowerCase()) {
         handleCombatAction(action);
